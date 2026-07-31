db1e109231f1b9b4a641d0e732e2a967ffc733d3
Meltem Köse <meltemkose@192.168.1.114>
2026-07-31 16:55:30 +0300

    Analiz dokümanı eklendi

---
+++ ANALIZ.md
@@ -0,0 +1,29 @@
+# Basit Görev Yönetim Sistemi - Analiz Dokümanı
+
+## Projenin Amacı
+Kullanıcıların projeler oluşturabildiği, görev ekleyebildiği ve görevlerin durumunu takip edebildiği web tabanlı bir görev yönetim sistemi geliştirmek.
+
+## Kullanıcı Rolleri
+- Admin
+- Kullanıcı
+
+## Sistem Özellikleri
+- Kullanıcı girişi
+- Kullanıcı yönetimi
+- Proje oluşturma
+- Görev oluşturma
+- Görev atama
+- Görev durumu güncelleme
+- Dashboard
+- Arama ve filtreleme
+
+## Kullanılacak Teknolojiler
+- ASP.NET Core MVC
+- SQL Server
+- Git
+- GitHub
+
+## Veritabanı Tabloları
+- Users
+- Projects
+- Tasks
--- obj/Debug/net8.0/GorevYonetimSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/GorevYonetimSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269127643503e3d71b67a29e448580ebcd69f94f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

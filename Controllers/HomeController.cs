3e937b9bd49bf4786e651c05a992ced2b3e9e580
Meltem Köse <meltemkose@192.168.1.35>
2026-08-08 14:34:10 +0300

    Added dashboard with task statistics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GorevYonetimSistemi.Models;
+using GorevYonetimSistemi.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GorevYonetimSistemi.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+private readonly ApplicationDbContext _context;
+   public HomeController(
+    ILogger<HomeController> logger,
+    ApplicationDbContext context)
+{
+    _logger = logger;
+    _context = context;
+}
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public async Task<IActionResult> Index()
+{
+    ViewBag.TotalTasks = await _context.Tasks.CountAsync();
+    ViewBag.PendingTasks = await _context.Tasks.CountAsync(t => t.Status == "Bekliyor");
+    ViewBag.InProgressTasks = await _context.Tasks.CountAsync(t => t.Status == "Devam Ediyor");
+    ViewBag.CompletedTasks = await _context.Tasks.CountAsync(t => t.Status == "Tamamlandı");
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+    return View();
+}
 
     public IActionResult Privacy()
     {
--- obj/Debug/net8.0/GorevYonetimSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/GorevYonetimSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918e7812b19d3ab0aed4880373404b50f6788149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b4286c78ebd80ce6900464cd03cab6b4c49d887")]
 [assembly: System.Reflection.AssemblyProductAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GorevYonetimSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
